3f4e548a7f156ca62b3501d18861622c40bec6bb
EugeneZhdanov <jeka7753191@gmail.com>
2021-08-03 16:45:23 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -10,13 +10,25 @@ import { v4 as uuidv4 } from 'uuid';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = contacts === null ? [] : JSON.parse(contacts);
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = (text, number) => {
     const contact = {
@@ -58,6 +70,7 @@ class App extends Component {
   };
 
   render() {
+    console.log('render');
     return (
       <Layout>
         <h1>PhoneBook</h1>
